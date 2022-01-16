f1bb737bb7af42efff71108780b921930f041741
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2022-01-16 23:03:47 +0100

    update about page

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
-
+import { Row, Col, Container } from 'reactstrap';
 
 export const About = () => {
     return (
         <div>
-            <h4 className="text-center">About</h4>
+            <Container className="bg-light p-5 rounded-lg mt-3 m-auto">
+                <Row>
+                    <Col>
+                        <h4>About me</h4>
+                        <p>Topspeed in water is below 2m/h</p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     )
 }
