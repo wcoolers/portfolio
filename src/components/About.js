a60482964bbca5b55390a9dba9f853f706faa700
Adegoke Akanbi <16087140+wcoolers@users.noreply.github.com>
2022-07-24 19:35:54 +0100

    added limit class to about page

--- src/components/About.js
+++ src/components/About.js
@@ -3,14 +3,14 @@ import { Row, Col, Container } from 'reactstrap';
 export const About = () => {
     return (
         <div>
-            <Container className="p-5 rounded-lg mt-3 m-auto">
+            <Container className="p-5 rounded-lg mt-3 m-auto limit">
                 <Row>
                     <Col>
                         <h4>About Me</h4>
-                        <p className='limit'>Digital art amazes me, and coding is like magic to me.
+                        <p>Digital art amazes me, and coding is like magic to me.
                             I divide my time between writing code, spending time with family and petting my fluffy cat, Femi. Well, he is a part of the family!
-                            </p>
-                        <p className='limit'>Sports: Top-speed in water (swimming) is below 1km/h 😁. I can also do a few backflips on land 😲.</p>
+                        </p>
+                        <p>Sports: Top-speed in water (swimming) is below 1km/h 😁. I can also do a few backflips on land 😲.</p>
                         <p>Connect with me on <a className='link' href="https://www.twitter.com/khentebe" target="_blank" rel="noreferrer">twitter</a> and <a className='link' href="https://www.linkedin.com/in/adegoke-a1/" target="_blank" rel="noreferrer">linkedin</a>.</p>
                         <p>Stay safe 👍. </p>
                     </Col>
